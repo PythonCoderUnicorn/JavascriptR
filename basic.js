5bc7d144d5c6f864a336619fec34e639dbd2f648
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-01-30 17:48:46 -0700

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,2 +1,18 @@
 
 // Javascript file
+
+// code that is here is from the p5.js online web code editor and pasted here for reference.
+
+
+//-------------------- starter code
+function setup() {
+  createCanvas(500, 500); // canvas dimension
+}
+
+function draw() {
+  background(100); // grey bg
+//-------------------
+  
+  //   x    y   w   ht
+  rect(230, 78, 56, 70);
+}
